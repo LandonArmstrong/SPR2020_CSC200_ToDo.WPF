7f26b94f52e457f303678967f40629a42faec25c
Landon.Armstrong <Landon.Armstrong@SMU11884>
2020-02-18 10:52:11 -0800

    Idk

--- ToDoAppWPF/Models/TodoTask.cs
+++ ToDoAppWPF/Models/TodoTask.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace ToDoApp.Wpf.Models
 {
+    [System.Serializable]
     class TodoTask
     {
         public string Description { get; set; }
         public bool IsComplete { get; set; }
+
+        public string Content { get; set; }
+
+        public System.DateTime LastSaved { get; set; }
+
+        public TodoTask()
+        {
+            Content = string.Empty;
+            LastSaved = System.DateTime.Now;
+        }
+        
+        
+
+        public TodoTask(string content)
+        {
+            Content = content;
+            LastSaved = System.DateTime.Now;
+        }
+
+
+
     }
+
 
 
     
